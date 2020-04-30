bc8aee1e7bd4fe8fdf7ab01d0ff180b815c8daff
Dorsey, Daniel <daniel_dorsey@manulife.ca>
2020-04-30 08:58:08 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parking app with Dan!!!</h1>
+      <form>
+        <label for="parking-spot">Parking spot number</label>
+        <input type="text" placeholder="C0-1" />
+        <label for="parking-spot">License plate number</label>
+        <input type="text" placeholder="N393N" />
+        <label for="parking-spot">Expiry time</label>
+        <input type="time" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
